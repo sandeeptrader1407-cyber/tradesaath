733d44762a7fb7d176ecab39349f1608dd935be4
Sandeep <sandeep.trader1407@gmail.com>
2026-05-10 11:21:11 +0530

    feat(sentry): integrate Sentry error tracking + session replay (Next.js wizard)

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://ff64b702462757e8404bc85d1a8aaa40@o4511024638459904.ingest.us.sentry.io/4511363841458176",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
